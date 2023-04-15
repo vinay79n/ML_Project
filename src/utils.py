d9673368a787e3f342495bf8b6c0dfa8d8ccaa53
Vinay Shende <vinayshende79@gmail.com>
2023-04-15 12:58:42 +0530

    deployment configuration

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,8 @@ import sys
 import dill
 import numpy as np
 import pandas as pd
+import pickle
+
 
 from sklearn.metrics import r2_score
 from src.exception import CustomException
@@ -47,5 +49,13 @@ def evaluate_model(X_train, y_train, X_test, y_test, models,param):
             report[list(models.keys())[i]] = test_model_score
 
             return report
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
     except Exception as e:
         raise CustomException(e, sys)
